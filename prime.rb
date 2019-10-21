3bfc88eedec07355044775cb5da47d53fb9cd793
Derric Fragosa <fderric@gmail.com>
2019-10-21 14:44:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  x = 2 
+  count = 0
+  until x < num
+      if  num % x == 0 || (2..num -1).none?
+       return "false"
+       count +=1 
+      
+      else 
+       return "true."
+      end 
+     
+ # is a prime number
+ #isn't a prime number
+   end 
+end 
+prime?(7)
